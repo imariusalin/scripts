80532aea6baaccbe94ae7df188288c47a9e696ff
imariusalin <59664196+imariusalin@users.noreply.github.com>
2021-12-24 12:24:24 +0200

    Update verifica-log4js.sh

--- verifica-log4js.sh
+++ verifica-log4js.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-	
-echo -e "\e[92mIncepere Scanare log4js"
-echo -e "\e[92mVerificam daca exita libraria log4js"
  find / -name log4j*.jar
  dpkg -l | grep liblog4j
  dpkg -l | grep log4
- echo -e "\e[92mVerificam tentative hack cu log4js"
- egrep -I -i -r '\$(\{|%7B)jndi:(ldap[s]?|rmi|dns|nis|iiop|corba|nds|http):/[^\n]+' /var/log - string cautare log-uri pt log4j
+ echo "Verificam loguri pentru incercari exploit log4js:\n"
+ egrep -I -i -r '\$(\{|%7B)jndi:(ldap[s]?|rmi|dns|nis|iiop|corba|nds|http):/[^\n]+' /var/log 
 
 while true; do
 read -rsn1 input
