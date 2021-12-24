2322dd7265f5edbf145e9a924c9a6523b500c697
imariusalin <59664196+imariusalin@users.noreply.github.com>
2021-12-24 12:46:04 +0200

    Update verifica-log4js.sh

--- verifica-log4js.sh
+++ verifica-log4js.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo "Apasa 1 pentru Scaner Secundar"
+echo "Verificam cu varianta manuala!"
  find / -name log4j*.jar
  dpkg -l | grep liblog4j
  dpkg -l | grep log4
@@ -12,6 +13,11 @@ if [ "$input" = "x" ]; then
     echo "Bye Bye!"
     exit
 fi
+
+if [ "$input" = "1" ]; then    
+    bash <(curl -s https://raw.githubusercontent.com/rubo77/log4j_checker_beta/main/log4j_checker_beta.sh)
+fi
+
 done
 
 
