a21bbf3885c4715e1d56cca4bcd14992e0875d3d
imariusalin <59664196+imariusalin@users.noreply.github.com>
2021-12-24 12:36:47 +0200

    Update verifica-log4js.sh

--- verifica-log4js.sh
+++ verifica-log4js.sh
@@ -4,7 +4,7 @@
  dpkg -l | grep liblog4j
  dpkg -l | grep log4
  echo "Verificam loguri pentru incercari exploit log4js:\n"
- egrep -I -i -r '\$(\{|%7B)jndi:(ldap[s]?|rmi|dns|nis|iiop|corba|nds|http):/[^\n]+' /var/log 
+ egrep -I -i -r '\$(\{|%7B)jndi:(ldap[s]?|rmi|dns|nis|iiop|corba|nds|http):/[^\n]+' /var/log
 
 while true; do
 read -rsn1 input
